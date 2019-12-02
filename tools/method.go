81ad4311a7bcaaa7a45038c1b08bd490fce4856b
lwnmengjing <991154416@qq.com>
2019-12-02 17:52:13 +0800

    微信官方支付支持

--- tools/method.go
+++ tools/method.go
@@ -2,49 +2,87 @@ package tools
 
 import (
 	"errors"
+	"github.com/chanxuehong/wechat/mch/core"
+	"github.com/chanxuehong/wechat/mch/pay"
 	"github.com/shop-r1/royalpay"
 	"github.com/shop-r1/sandpay"
-	"github.com/shop-r1/sandpay/pay"
+	spay "github.com/shop-r1/sandpay/pay"
 	"github.com/shop-r1/sandpay/pay/params"
 	"strconv"
 	"time"
 )
 
-var PaymentMethod = map[string]func(key, secret, image, orderId string, body *royalpay.Body) (*royalpay.Result, error){
+var PaymentMethod = map[string]func(key, secret, appId, image, orderId string, body *royalpay.Body, params1 ...string) (*royalpay.Result, error){
 	"RoyalPayWechat": RoyalPayWechat,
 	"RoyalPayAlipay": RoyalPayAliapay,
 	"WechatVoucher":  Voucher,
 	"Overage":        Overage,
 	"SandPayAliapay": SandPayAliapay,
+	"WechatOfficial": WechatOfficial,
 }
 
-func RoyalPayWechat(key, secret, image, orderId string, body *royalpay.Body) (result *royalpay.Result, e error) {
+func WechatOfficial(key, secret, appId, image, orderId string, body *royalpay.Body, params1 ...string) (result *royalpay.Result, e error) {
+	client := core.NewClient(appId, key, secret, nil)
+	req := &pay.UnifiedOrderRequest{
+		Body:           body.Description,
+		OutTradeNo:     orderId,
+		TotalFee:       int64(body.Price),
+		SpbillCreateIP: "127.0.0.1",
+		NotifyURL:      body.NotifyUrl,
+		TradeType:      "JSAPI",
+		OpenId:         body.Operator,
+	}
+	if len(params1) > 0 {
+		req.SpbillCreateIP = params1[0]
+	}
+	resp, err := pay.UnifiedOrder2(client, req)
+	if err != nil {
+		return nil, err
+	}
+	timeStamp := strconv.Itoa(int(time.Now().Unix()))
+	nonceStr := strconv.Itoa(int(time.Now().UnixNano()))
+	signType := core.SignType_MD5
+	paySign := core.JsapiSign(appId, timeStamp, nonceStr, resp.PrepayId, signType, secret)
+	result = &royalpay.Result{}
+
+	result.Params = map[string]interface{}{
+		"appId":     appId,
+		"timeStamp": timeStamp,
+		"nonceStr":  nonceStr,
+		"package":   "prepay_id=" + resp.PrepayId,
+		"signType":  signType,
+		"paySign":   paySign,
+	}
+	return
+}
+
+func RoyalPayWechat(key, secret, appId, image, orderId string, body *royalpay.Body, params1 ...string) (result *royalpay.Result, e error) {
 	if key == "" || secret == "" {
 		return nil, errors.New("商家对该支付方式未配置")
 	}
-	pay := royalpay.NewPay(key, secret)
+	pay2 := royalpay.NewPay(key, secret)
 	body.Channel = royalpay.Wechat
-	result, e = pay.QrcodeOrder(orderId, body)
+	result, e = pay2.QrcodeOrder(orderId, body)
 	if e == nil && len(body.Redirect) > 10 {
-		result.CodeUrl = pay.Redirect(result.CodeUrl, body.Redirect)
+		result.CodeUrl = pay2.Redirect(result.CodeUrl, body.Redirect)
 	}
 	return result, e
 }
 
-func RoyalPayAliapay(key, secret, image, orderId string, body *royalpay.Body) (result *royalpay.Result, e error) {
+func RoyalPayAliapay(key, secret, appId, image, orderId string, body *royalpay.Body, params1 ...string) (result *royalpay.Result, e error) {
 	if key == "" || secret == "" {
 		return nil, errors.New("商家对该支付方式未配置")
 	}
-	pay := royalpay.NewPay(key, secret)
+	pay2 := royalpay.NewPay(key, secret)
 	body.Channel = royalpay.Alipay
-	return pay.QrcodeNativeOrder(orderId, body)
+	return pay2.QrcodeNativeOrder(orderId, body)
 }
 
-func SandPayAliapay(key, secret, image, orderId string, body *royalpay.Body) (result *royalpay.Result, e error) {
+func SandPayAliapay(key, secret, appId, image, orderId string, body *royalpay.Body, params1 ...string) (result *royalpay.Result, e error) {
 	if key == "" {
 		return nil, errors.New("商家对该支付方式未配置")
 	}
-	sandpay.Client.Config = pay.Config{
+	sandpay.Client.Config = spay.Config{
 		Version:         "1.0",
 		MerId:           key,
 		PrivatePath:     "certs/server.key", //私钥文件
@@ -54,7 +92,7 @@ func SandPayAliapay(key, secret, image, orderId string, body *royalpay.Body) (re
 		NotifyUrl:       body.NotifyUrl,
 		FrontUrl:        body.Redirect,
 	}
-	params := params.OrderPayParams{
+	payParams := params.OrderPayParams{
 		OrderNo:     strconv.Itoa(int(time.Now().UnixNano())),
 		TotalAmount: body.Price,
 		Subject:     body.Description,
@@ -67,14 +105,14 @@ func SandPayAliapay(key, secret, image, orderId string, body *royalpay.Body) (re
 			OpenId:   "xxx",
 		},
 	}
-	e = pay.LoadCertInfo(&sandpay.Client.Config)
+	e = spay.LoadCertInfo(&sandpay.Client.Config)
 	if e != nil {
 		return nil, e
 	}
 	sandPay := &sandpay.SandPay{
 		Config: sandpay.Client.Config,
 	}
-	gotResp, err := sandPay.OrderPayQrAlipay(params)
+	gotResp, err := sandPay.OrderPayQrAlipay(payParams)
 	if err != nil {
 		return nil, err
 	}
@@ -85,7 +123,7 @@ func SandPayAliapay(key, secret, image, orderId string, body *royalpay.Body) (re
 	return result, nil
 }
 
-func Voucher(key, secret, image, orderId string, body *royalpay.Body) (result *royalpay.Result, e error) {
+func Voucher(key, secret, appId, image, orderId string, body *royalpay.Body, params1 ...string) (result *royalpay.Result, e error) {
 	result = &royalpay.Result{
 		ReturnCode: "SUCCESS",
 		OrderId:    orderId,
@@ -95,7 +133,7 @@ func Voucher(key, secret, image, orderId string, body *royalpay.Body) (result *r
 	return result, nil
 }
 
-func Overage(key, secret, image, orderId string, body *royalpay.Body) (result *royalpay.Result, e error) {
+func Overage(key, secret, appId, image, orderId string, body *royalpay.Body, params1 ...string) (result *royalpay.Result, e error) {
 	result = &royalpay.Result{
 		ReturnCode: "SUCCESS",
 		OrderId:    orderId,
