f289ec03dc6787f3f2e2f86613ea2054b25de064
lwnmengjing <991154416@qq.com>
2019-12-04 20:55:03 +0800

    添加品牌ID

--- models/goods.go
+++ models/goods.go
@@ -15,6 +15,7 @@ type Goods struct {
 	TenantId             string               `gorm:"primary_key" sql:"type:char(20);index" description:"租户ID" json:"-" `
 	CategoryId           string               `sql:"type:char(20);index" json:"category_id"`
 	ParentCategoryId     string               `sql:"type:char(20);index" json:"parent_category_id"`
+	BrandId              string               `sql:"type:char(20);index" json:"brand_id"`
 	Used                 bool                 `description:"领用" json:"used"`
 	GoodsInfoId          string               `sql:"type:char(20);index" json:"goods_info_id" description:"商品基础信息ID"`
 	GoodsInfo            GoodsInfo            `gorm:"save_associations:false" json:"goods_info" validate:"-"`
