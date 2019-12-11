341e51f98e9841b837353a3a5d6e67ce4a7e2825
lwnmengjing <991154416@qq.com>
2019-12-11 09:02:25 +0800

    新增配置表

--- models/config.go
+++ models/config.go
@@ -1,10 +1,31 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"encoding/json"
+	"github.com/jinzhu/gorm"
+)
 
 type SystemConfig struct {
 	gorm.Model
+	TenantId string                 `sql:"type:char(20);index" description:"租户ID" json:"tenant_id"`
 	Name     string                 `sql:"type:varchar(100);index" description:"名称" json:"name" validate:"required"`
 	Meta     map[string]interface{} `sql:"-" description:"数据集合" json:"meta"`
 	Metadata []byte                 `sql:"type:json" description:"数据集合" json:"-"`
+}
+
+func (e *SystemConfig) BeforeSave() (err error) {
+	if len(e.Meta) == 0 || e.Meta == nil {
+		e.Metadata = []byte(`{}`)
+	} else {
+		e.Metadata, err = json.Marshal(e.Meta)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (e *SystemConfig) BeforeFind() (err error) {
+	err = json.Unmarshal(e.Metadata, &e.Meta)
+	return nil
 }
