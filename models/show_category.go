40b2d5809d05f4ab7729b4fc8671840b0460b0d0
lwnmengjing <991154416@qq.com>
2019-12-20 09:36:18 +0800

    调整活动详情查询

--- models/show_category.go
+++ models/show_category.go
@@ -36,6 +36,24 @@ func (ShowCategoryParentGoods) TableName() string {
 	return "show_categories"
 }
 
+type ShowCategoryGoods struct {
+	gorm.Model
+	No          string  `sql:"-" json:"id"`
+	TenantId    string  `sql:"type:char(20);index" description:"租户ID" json:"-"`
+	Name        string  `sql:"type:varchar(100)" description:"名称" json:"name" validate:"required"`
+	Image       string  `json:"image"`
+	ParentId    string  `sql:"type:char(20);index" json:"parent_id" description:"父级ID"`
+	Status      Status  `sql:"type:integer;default(1);index" description:"展示状态" json:"status" validate:"required"`
+	Description string  `sql:"type:text" description:"描述" json:"description"`
+	Sort        int     `description:"排序" json:"sort"`
+	Goods       []Goods `gorm:"ForeignKey:ShowCategoryId" json:"goods"`
+	PGoods      []Goods `gorm:"ForeignKey:ParentShowCategoryId" json:"p_goods"`
+}
+
+func (ShowCategoryGoods) TableName() string {
+	return "show_categories"
+}
+
 func (s *ShowCategory) AfterSave() error {
 	s.transform()
 	return nil
